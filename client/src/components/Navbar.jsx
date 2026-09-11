376b5d99897f27071228f3c64d4ec3b0f5718f16
Nithya Shaji <nithyashaji2006@gmail.com>
2026-09-11 11:03:19 +0530

    Add shared frontend UI foundation

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">FarmersBook</Link>
+      </div>
+
+      <div className="navbar-links">
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/lands">Lands</Link>
+        <Link to="/profile">Profile</Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
